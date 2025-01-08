e9998b454c06fa195ca72258c5ffa38d44c30067
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-01-08 14:04:33 +0700

    upfate page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,30 +2,31 @@
 
 import { Settings } from "lucide-react";
 import { useState } from "react";
-import { useGetSurah } from "@/api/allSurah";
+import { useGetSurah, useGetDetailSurahByNomor } from "@/api/allSurah";
 import ListSurah from "./components/listSurah";
+import DetailSurahQuran from "./components/detailSurah";
 
 export default function Home() {
   const [activeCard, setActiveCard] = useState(1);
-  const { data } = useGetSurah();
-  //   const { data: detailSurah } = useGetDetailSurahByNomor(activeCard);
-  console.log(data);
+  const { data, isLoading } = useGetSurah();
+  const { data: detailSurah, isLoading: detailSurahLoading } =
+    useGetDetailSurahByNomor(activeCard);
 
   return (
-    <div className="bg-red-100 min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <div className="flex justify-between items-center bg-white w-full p-4">
         <h1 className="text-2xl font-bold">My Quran</h1>
         <Settings size={24} />
       </div>
 
-      {/* Main Content */}
       <div className="grid grid-cols-[22%_78%] flex-grow">
         <ListSurah
           data={data}
           setActiveCard={setActiveCard}
           activeCard={activeCard}
+          isLoading={isLoading}
         />
-        <div className="bg-white border border-l p-4">{activeCard}</div>
+        <DetailSurahQuran data={detailSurah} isLoading={detailSurahLoading} />
       </div>
     </div>
   );
