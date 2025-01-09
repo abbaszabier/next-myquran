658f394863eb65f3425f4296cdcdbfd8a23b1289
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-01-09 13:49:11 +0700

    feat add command dialog

--- src/app/(home)/components/listSurah.tsx
+++ src/app/(home)/components/listSurah.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Card,
   CardDescription,
@@ -21,24 +21,38 @@ const ListSurah: React.FC<ListSurahProps> = ({
   setActiveCard,
   activeCard,
 }) => {
+  const [isMac, setIsMac] = useState(false);
   const handleCardClick = (nomor: number) => {
     setActiveCard(nomor);
   };
+
+  useEffect(() => {
+    const platform = window.navigator.platform.toLowerCase();
+    setIsMac(platform.includes("mac"));
+  }, []);
 
   if (isLoading) return <LoadingListSurah />;
 
   return (
     <div className="bg-white space-y-4 overflow-y-auto max-h-[calc(100vh-4rem)] border border-r-0">
       <div className="flex items-center justify-center sticky top-0 z-100 justify-between items-center h-[80px] px-4 bg-white border-b border-gray-200 backdrop-blur-sm bg-opacity-80 backdrop-filter backdrop-saturate-150">
-        <div className="bg-gray-100 p-2 px-3 rounded-lg flex items-center justify-center w-full gap-2 border border-gray-200">
+        <div className="bg-gray-50 p-2 px-3 rounded-lg flex items-center justify-center w-full gap-2 border border-gray-200">
           <input
             type="text"
             placeholder="Search Surah..."
             className="flex-grow bg-transparent outline-none"
           />
-          <span className="text-xs p-1 border border-gray-100 bg-gray-200 rounded-md">
-            ⌘ K
-          </span>
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            {isMac ? (
+              <>
+                <span className="text-xs">⌘</span>K
+              </>
+            ) : (
+              <>
+                <span className="text-xs">CTRL</span>K
+              </>
+            )}
+          </kbd>
         </div>
       </div>
       {data?.map((surah, index) => (
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,57 @@
 "use client";
 
 import { Settings } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useGetSurah, useGetDetailSurahByNomor } from "@/api/allSurah";
 import ListSurah from "./components/listSurah";
 import DetailSurahQuran from "./components/detailSurah";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
   const [activeCard, setActiveCard] = useState(1);
   const { data, isLoading } = useGetSurah();
   const { data: detailSurah, isLoading: detailSurahLoading } =
     useGetDetailSurahByNomor(activeCard);
 
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="flex justify-between items-center bg-white w-full p-4">
-        <h1 className="text-2xl font-bold">My Quran</h1>
+        <div className="flex items-center space-x-4">
+          <h1 className="text-2xl font-bold">QuranKu</h1>
+
+          <nav className="flex space-x-4">
+            <a href="#quran" className="text-md text-black">
+              Quran
+            </a>
+            <a href="#jadwal-shalat" className="text-md">
+              Shalat
+            </a>
+            <a href="#jadwal-shalat" className="text-md">
+              Tersimpan
+            </a>
+          </nav>
+        </div>
+
         <Settings size={24} />
       </div>
 
@@ -28,6 +64,29 @@ export default function Home() {
         />
         <DetailSurahQuran data={detailSurah} isLoading={detailSurahLoading} />
       </div>
+
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <DialogTitle className="hidden"></DialogTitle>
+        <CommandInput placeholder="Type a surah..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="List of surah">
+            {data?.map((surah) => (
+              <CommandItem
+                key={surah.nomor}
+                onSelect={() => {
+                  console.log(`Surah selected: ${surah.nomor}`);
+                  setActiveCard(surah.nomor);
+                  setOpen(false);
+                }}
+                className="cursor-pointer"
+              >
+                {surah.namaLatin}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
     </div>
   );
 }
