9578938742c1b0379b9dfa04eeb0422e5bb76816
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-01-30 14:26:46 +0700

    update detail surah

--- src/app/(home)/components/detailSurah.tsx
+++ src/app/(home)/components/detailSurah.tsx
@@ -12,6 +12,7 @@ import { ChevronLeft, Pause, Play } from "lucide-react";
 import { useSettingsStore } from "@/store/settings";
 import { useRef, useState } from "react";
 import { SelectAyat } from "./selectAyat";
+import DropdownDetailQuran from "./dropdownDetailQuran";
 
 interface DetailSurahProps {
   data?: DetailSurah;
@@ -119,72 +120,76 @@ const DetailSurahQuran: React.FC<DetailSurahProps> = ({
             setIsPlaying(false); // Gagal memutar audio
           });
       }
+
+      audioPlayer.current.addEventListener("ended", () => {
+        setIsPlaying(false);
+      });
     }
   };
 
   if (isLoading) return <LoadingDetailSurah />;
 
   return (
-    <div className="border borrder-gray-200 space-y-4">
-      <div className="flex sticky top-0 z-100 justify-between items-center h-[80px] px-4 bg-white border-b border-gray-200 backdrop-blur-sm bg-opacity-80 backdrop-filter backdrop-saturate-150">
+    <div className="md:border-l border-b border-gray-200 dark:border-gray-800 space-y-4">
+      <div className="flex sticky top-0 z-100 justify-between items-center h-[60px] md:h-[80px] px-4 bg-white dark:bg-black border-b border-gray-200 dark:border-gray-800 backdrop-blur-sm bg-opacity-80 backdrop-filter backdrop-saturate-150 dark:backdrop-blur-md dark:bg-opacity-60">
         {/* Left Navigation */}
-        <div className="flex gap-2 items-center">
-          <button
-            aria-label="Surah Sebelumnya"
-            title={`Surah Sebelumnya: ${
-              typeof data?.suratSebelumnya === "object"
-                ? data.suratSebelumnya.namaLatin
-                : "-"
-            }`}
-            onClick={() => data?.nomor !== undefined && prevSurah(data.nomor)}
-            className={`border border-gray-200 p-1 shadow-sm rounded cursor-pointer hover:bg-gray-50 ${
-              !data?.suratSebelumnya ? "cursor-not-allowed opacity-50" : ""
-            }`}
-          >
-            <ChevronLeft size={24} />
-          </button>
-          <button
-            aria-label="Surah Selanjutnya"
-            title={`Surah Selanjutnya: ${
-              typeof data?.suratSelanjutnya === "object"
-                ? data.suratSelanjutnya.namaLatin
-                : "-"
-            }`}
-            onClick={() => data?.nomor !== undefined && nextSurah(data.nomor)}
-            className={`border border-gray-200 p-1 shadow-sm rounded cursor-pointer transform rotate-180 hover:bg-gray-50 ${
-              !data?.suratSelanjutnya ? "cursor-not-allowed opacity-50" : ""
-            }`}
-          >
-            <ChevronLeft size={24} />
-          </button>
-          <SelectAyat onSelectAyat={onSelectAyat} data={data?.ayat} />
-        </div>
+        <div className="flex gap-4 items-center">
+          {/* Navigation mobile */}
+          <div className="flex gap-2 items-center md:hidden">
+            <button
+              aria-label="Surah Sebelumnya"
+              title={`Surah Sebelumnya: ${
+                typeof data?.suratSebelumnya === "object"
+                  ? data.suratSebelumnya.namaLatin
+                  : "-"
+              }`}
+              onClick={() => data?.nomor !== undefined && prevSurah(data.nomor)}
+              className={`border border-gray-300 p-2 rounded-lg hover:bg-gray-100 transition ${
+                !data?.suratSebelumnya ? "cursor-not-allowed opacity-50" : ""
+              }`}
+            >
+              <ChevronLeft size={14} />
+            </button>
+            <button
+              aria-label="Surah Selanjutnya"
+              title={`Surah Selanjutnya: ${
+                typeof data?.suratSelanjutnya === "object"
+                  ? data.suratSelanjutnya.namaLatin
+                  : "-"
+              }`}
+              onClick={() => data?.nomor !== undefined && nextSurah(data.nomor)}
+              className={`border border-gray-300 p-2 rounded-lg hover:bg-gray-100 transition transform rotate-180 ${
+                !data?.suratSelanjutnya ? "cursor-not-allowed opacity-50" : ""
+              }`}
+            >
+              <ChevronLeft size={14} />
+            </button>
+          </div>
 
-        {/* Center Controls */}
-        <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold">{data?.namaLatin}</h1>
-          <div className="flex justify-center items-center space-x-4">
-            <p className="text-sm">
-              {data?.arti} ⊙ {data?.jumlahAyat} Ayat ⊙ {data?.tempatTurun}
-            </p>
+          <div className="flex flex-col items-left">
+            <h1 className="text-md md:text-xl font-bold">{data?.namaLatin}</h1>
+            <div className="flex justify-center items-center space-x-4">
+              <p className="text-xs md:text-sm">
+                {data?.arti} ⊙ {data?.jumlahAyat} Ayat
+              </p>
+            </div>
           </div>
         </div>
 
         {/* Right Navigation */}
-        <div className="flex w-[180px] gap-2 items-center">
+        <div className="flex gap-2 items-center hidden md:flex">
           <button
-            aria-label="Play Full Audio"
-            title="Play Full Audio"
+            aria-label="Play Audo"
             onClick={playAudio}
-            className="flex items-center p-2 bg-foreground text-white rounded hover:bg-blue-600 transition"
+            className={`flex items-center p-2 rounded transition bg-gray-200 text-gray-800 hover:bg-gray-300`}
           >
             {isPlaying ? <Pause size={16} /> : <Play size={16} />}
           </button>
-          <Select onValueChange={onChaneQori} defaultValue={qori}>
-            <SelectTrigger className="w-[140px]" aria-label="Pilih Qori">
-              <SelectValue placeholder="Pilih Qori" aria-label="Pilih Qori" />
+          <Select onValueChange={onChaneQori}>
+            <SelectTrigger className="w-[140px]" aria-label="Pilih qori">
+              <SelectValue placeholder="Pilih qori" aria-label="Pilih qori" />
             </SelectTrigger>
-            <SelectContent className="w-[140px]" aria-label="Pilih Qori">
+            <SelectContent className="w-[140px]" aria-label="Pilih qori">
               {Object.keys(data?.audioFull ?? {}).map((item, i) => (
                 <SelectItem
                   key={i}
@@ -196,9 +201,54 @@ const DetailSurahQuran: React.FC<DetailSurahProps> = ({
               ))}
             </SelectContent>
           </Select>
+          <SelectAyat onSelectAyat={onSelectAyat} data={data?.ayat} />
+          <div className="flex gap-2 items-center">
+            <button
+              aria-label="Surah Sebelumnya"
+              title={`Surah Sebelumnya: ${
+                typeof data?.suratSebelumnya === "object"
+                  ? data.suratSebelumnya.namaLatin
+                  : "-"
+              }`}
+              onClick={() => data?.nomor !== undefined && prevSurah(data.nomor)}
+              className={`border border-gray-300 p-2 rounded-lg hover:bg-gray-100 transition ${
+                !data?.suratSebelumnya ? "cursor-not-allowed opacity-50" : ""
+              }`}
+            >
+              <ChevronLeft size={20} />
+            </button>
+            <button
+              aria-label="Surah Selanjutnya"
+              title={`Surah Selanjutnya: ${
+                typeof data?.suratSelanjutnya === "object"
+                  ? data.suratSelanjutnya.namaLatin
+                  : "-"
+              }`}
+              onClick={() => data?.nomor !== undefined && nextSurah(data.nomor)}
+              className={`border border-gray-300 p-2 rounded-lg hover:bg-gray-100 transition transform rotate-180 ${
+                !data?.suratSelanjutnya ? "cursor-not-allowed opacity-50" : ""
+              }`}
+            >
+              <ChevronLeft size={20} />
+            </button>
+          </div>
+        </div>
+
+        {/* Right Navigation Mobile */}
+        <div className="flex gap-2 items-center md:hidden">
+          <DropdownDetailQuran
+            darkMode={false}
+            toggleDarkMode={() => {}}
+            isPlaying={isPlaying}
+            playAudio={playAudio}
+            onChaneQori={onChaneQori}
+            data={data}
+            getUserNameQori={getUserNameQori}
+            onSelectAyat={onSelectAyat}
+          />
         </div>
       </div>
-      <div className="px-4 pb-4 space-y-4">
+      <div className="px-4 md:pb-4 pb-20 space-y-4">
         {data?.ayat.map((verse) => (
           <div key={verse.nomorAyat}>
             <QuranVerseCard
