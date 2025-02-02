863186704d5f3ea67f8ec00f9e2db5a8029d4e8c
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-02-02 18:51:30 +0700

    implement quran offline

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useGetSurah, useGetDetailSurahByNomor } from "@/api/allSurah";
+import {
+  useGetSurah,
+  useGetDetailSurahByNomor,
+  Surah,
+  DetailSurah,
+} from "@/api/allSurah";
 import ListSurah from "./components/listSurah";
 import DetailSurahQuran from "./components/detailSurah";
 import {
@@ -13,13 +18,51 @@ import {
 } from "@/components/ui/command";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { useSettingsStore } from "@/store/settings";
+import { db } from "@/lib/db";
+import { useLiveQuery } from "dexie-react-hooks";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
-  const { data, isLoading } = useGetSurah();
   const { activeCard, setActiveCard } = useSettingsStore((state) => state);
+  const online = typeof window !== "undefined" ? navigator.onLine : true;
+
+  // List Surah
+  const { data, isLoading } = useGetSurah(
+    online ? { enabled: online } : { enabled: false }
+  );
+
+  useEffect(() => {
+    if (online) {
+      const saveDataListSurah: Surah[] = data ?? [];
+
+      db.listSurah.bulkPut(saveDataListSurah).then(() => {
+        console.log("Data surah berhasil disimpan");
+      });
+    }
+  }, [data, online]);
+
+  const listSurah = useLiveQuery(() => db.listSurah.toArray(), []);
+
+  // Detail Surah
   const { data: detailSurah, isLoading: detailSurahLoading } =
     useGetDetailSurahByNomor(activeCard);
+
+  useEffect(() => {
+    if (online && activeCard) {
+      if (detailSurah) {
+        const saveDataDetailSurah: DetailSurah = detailSurah;
+        db.detailSurah.bulkPut([saveDataDetailSurah]).then(() => {
+          console.log("Data detail surah berhasil disimpan");
+        });
+      }
+    }
+  }, [detailSurah, online, activeCard]);
+
+  const detailSurahFromDb = useLiveQuery(() => db.detailSurah.toArray(), []);
+
+  const detailSurahFilter = detailSurahFromDb?.find(
+    (surah) => surah.nomor === activeCard
+  );
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -36,7 +79,7 @@ export default function Home() {
     <div className="min-h-screen flex flex-col">
       <div className="grid grid-cols-1 lg:grid-cols-[22%_78%] flex-grow">
         <ListSurah
-          data={data}
+          data={online ? data : listSurah}
           setActiveCard={setActiveCard}
           activeCard={activeCard}
           isLoading={isLoading}
@@ -44,7 +87,7 @@ export default function Home() {
         />
         <DetailSurahQuran
           setActiveCard={setActiveCard}
-          data={detailSurah}
+          data={online ? detailSurah : detailSurahFilter}
           isLoading={detailSurahLoading}
           activeCard={activeCard}
         />
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,22 @@
+import { DetailSurah, Surah } from "@/api/allSurah";
 import { Shalat } from "@/api/shalat";
+import { Tafsir } from "@/api/tafsir";
 import Dexie, { type EntityTable } from "dexie";
 
 export const db = new Dexie("QuranKu") as Dexie & {
   jadwalShalatHarian: EntityTable<Shalat, "id">;
   jadwalShalatBulanan: EntityTable<Shalat, "id">;
+  listSurah: EntityTable<Surah, "nomor">;
+  detailSurah: EntityTable<DetailSurah, "nomor">;
+  tafsir: EntityTable<Tafsir, "nomor">;
 };
 
-db.version(1).stores({
+db.version(2).stores({
   jadwalShalatHarian: "++id, lokasi, daerah, jadwal",
   jadwalShalatBulanan: "++id, lokasi, daerah, jadwal",
+  listSurah:
+    "++id, nomor, nama, namaLatin, jumlahAyat, tempatTurun, arti, deskripsi, audioFull",
+  detailSurah:
+    "++id, arti, audioFull, ayat, deskripsi, jumlahAyat, nama, namaLatin, nomor, suratSebelumnya, suratSelanjutnya, tempatTurun",
+  tafsir: "++id, nomor, tafsir",
 });
